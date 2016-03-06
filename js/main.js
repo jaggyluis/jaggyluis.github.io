1c1b60889ba2799d004891e0023f1b816f6e2398
unknown <luis.jaggy@gmail.com>
2016-03-06 08:57:43 -0800

    refactor: implementing new site styles

--- js/main.js
+++ js/main.js
@@ -1,19 +1,94 @@
-'strict mode'
+/*global main */
+var main = main || {};
 
-function toggleFeatured(){
-	$(".featured-img").toggleClass("collapsed");
-	$(".featured").hover(
-		function(){
-			$(this).find(".featured-img").animate({height: "80px"}, "fast");
-		},
-		function(){
-			$(this).find(".featured-img").animate({height: "0px"}, "fast");
-		});
-}
+(function() {
+	'use strict';
 
-function main(){
-	toggleFeatured();
-}
+	main.init = function() {
+
+		this.title = 'luis michel jaggy'
+		this.footerItems = ko.observableArray(footerItems);
+		this.navItems = ko.observableArray(navItems);
+		this.projects = ko.observableArray();
+
+		ko.applyBindings(main);
+
+		this.setNavItems();
+		console.log(this.projects());
+
+	},
+	main.getTitle = function() {
+		return this.title.split(' ');
+	},
+	main.setNavItems = function() {
+		/*
+		 * TODO	- move a lot of this into knockout
+		 * 		- split out functionality for reuse
+		 */
+		for (var i=0; i<this.navItems().length; i++){
+
+			var navItem = this.navItems()[i];
+			var DOMelement = document.getElementById(navItem.name);
+
+			var header = document.createElement('div');
+			var content = document.createElement('div');
+
+			var self = this;
 
 
-$(document).ready(main);
+			header.innerHTML = this.getNavItemName(navItem);
+			header.classList.add('nav-item-header');
+			header.addEventListener('click', function() {
+				this.classList.toggle('open');
+				if (this.classList.contains('open')){
+					this.innerText = this.innerText.replace('+', '-');
+				} else {
+					this.innerText = this.innerText.replace('-', '+');
+				}
+				this.nextSibling.classList.toggle('hidden');
+			});
+		
+			
+			if (Math.random()  < 0.5 ) {
+				//DOMelement.classList.add('outlined-right');
+				DOMelement.classList.add('outlined-left');
+			} else {
+				DOMelement.classList.add('outlined-left');
+			}
+
+			DOMelement.appendChild(header);	
+			DOMelement.appendChild(content);
+
+			
+			content.classList.add('hidden');
+			content.classList.add('nav-item-content');
+
+			
+			navItem.content.forEach(function(contentItem) {
+				var item = document.createElement('div');
+				item.classList.add('nav-item-content-el')
+				//item.classList.add('outlined-top')
+				//item.classList.add('outlined-right')
+				if (Math.random()  < 0.5 ) {
+					//item.classList.add('outlined-right');
+					item.classList.add('outlined-left');
+				} else {
+					item.classList.add('outlined-left');
+				}
+				item.id = contentItem.name;
+				item.innerHTML = contentItem.name;
+				content.appendChild(item);
+
+				self.projects().push(contentItem);
+			});			
+		}
+			
+	},
+	main.getNavItemName = function(navItem) {
+		if (navItem.content.length > 0) { return navItem.name + ' +'; }
+		else{ return navItem.name; }
+	};
+
+	main.init();
+
+})();
---
+++ js/model.js
@@ -0,0 +1,86 @@
+
+
+var navItems = [
+	{
+		name: 'design',
+		url: null,
+		content: [
+			{
+				name: 'the feed',
+				url: 'pages/the_feed'
+			},
+			{
+				name: 'bauhaus museum',
+				url: 'pages/bauhaus'
+			},
+			{
+				name: 'innovatrium',
+				url: 'pages/bauhaus'
+			},
+			{
+				name: 'vertial vities asia',
+				url: 'pages/bauhaus'
+			},
+			{
+				name: 'glen park nursery school',
+				url: 'pages/bauhaus'
+			}
+		],
+	},
+	{
+		name: 'computation',
+		url: null,
+		content: [
+			{
+				name: 'design modeling symposium',
+				url: 'pages/studio_one'
+			},
+			{
+				name: 'decodes',
+				url: 'pages/studio_one'
+			},
+			{
+				name: 'fieldguide to computation',
+				url: 'pages/studio_one'
+			},
+			{
+				name: 'ddmi',
+				url: 'pages/studio_one'
+			},
+			{
+				name: 'panther game engine',
+				url: 'pages/studio_one'
+			}
+		],
+	},
+	{
+		name: 'teaching',
+		url: null,
+		content: [
+		],
+	},
+	{
+		name: 'misc',
+		url: null,
+		content: [
+		],
+	},
+
+];
+
+var footerItems = [
+	{
+		name: 'linkedIn',
+		url: null,
+		img: 'img/linkedin.jpg'
+	},
+	{
+		name: 'contact',
+		url: null,
+	},
+	{
+		name: 'resume',
+		url: null,
+	}
+]
+
